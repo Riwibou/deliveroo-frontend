02da8ebc1432bdfbd88fef197cd731991f6dfb0d
Riwan William Bououli <riwan.b@live.fr>
2024-04-22 17:50:39 +0200

    changement d etoile pour un svg

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,9 @@ function App() {
                               <span className="resto-descr">{meal.price} €</span>
                               {meal.popular &&
                                 <span className="pop">
-                                  <img className="etoile" src="../public/etoile.png" alt="etoile" /> Populaire
+                                  <svg className="etoile" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+                                  </svg> Populaire
                                 </span>
                               }
                             </div>
